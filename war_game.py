b89fa02ffe6185ba92e43542c24f32f7cd8b431f
Diksha Paliwal <51503505+dikshap07@users.noreply.github.com>
2021-04-20 23:33:30 +0530

    Add files via upload

---
+++ war_game.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 23:30:17 2021
+
+@author: Dikhsha
+"""
+
+from card_class import Card
+
+two_hearts = Card('Hearts','2')
+
+print(two_hearts.__str__())
